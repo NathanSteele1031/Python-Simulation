26ca3cedd1263334d9b8574cbdee1f4f2dd16b23
Nathan Steele <nsteele1031@gmail.com>
2026-05-24 20:33:07 -0400

    Added the show function and fixed a bug

--- Map/map.py
+++ Map/map.py
@@ -1,19 +1,25 @@
-from tile import Tile
+from tile import Tile, WorldObject
 
 class Map:
     
-    def __init__(self, width):
+    def __init__(self, width: int):
         self.width = width
         self.tiles = []
         self.__set_tiles__()
 
     def __set_tiles__(self):
-        for i in range(self.width):
+        for i in range(self.width*self.width):
             self.tiles.append(Tile())
         
-    def add_object(self, index, given_object):
+    def add_object(self, index: int, given_object: WorldObject):
         self.tiles[index].add_object(given_object)
         
-        
+    def remove_object(self, index: int, given_object: WorldObject):
+        self.tiles[index].remove_object(given_object)
+
     def show(self):
-        pass
+        for y in range(self.width):
+            for x in range(self.width):
+                selected_index = x + (y*self.width)
+                print(self.tiles[selected_index].show(), end="")
+            print()
