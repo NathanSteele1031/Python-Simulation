4220611487d853b93d269aa0f6f0ca5373b05ae6
NathanSteele1031 <nsteele1031@gmail.com>
2026-05-26 17:09:19 -0400

    Added seed option to plants

--- Plants/plant.py
+++ Plants/plant.py
@@ -1,9 +1,14 @@
 from Map.world_object import WorldObject
 
 class Plant(WorldObject):
-    def __init__(self, name: str):
+    def __init__(self, name: str, seed: bool):
         super().__init__(name)
         self.age = 0
+        self.seed = seed
     
     def age_up(self):
         self.age += 1
+    
+    def grown(self):
+        if self.seed and self.age == 5:
+            self.seed = not self.seed
