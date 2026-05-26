3eac398329677019bf4b22e5c407495f66ed0e79
NathanSteele1031 <nsteele1031@gmail.com>
2026-05-26 15:28:50 -0400

    Made it a class of functions.

--- Plants/plant_manager.py
+++ Plants/plant_manager.py
@@ -3,14 +3,13 @@ from Map.map import Map
 from Map.tile import Tile
 
 class PlantManager:
-    def update(self, given_map: Map):
+    def update(given_map: Map):
         for selected_tile in given_map.tiles:
-            if selected_tile.has_plant():
-                plant_objects = self.get_plants(selected_tile)
-                for selected_plant in plant_objects:
-                    selected_plant.age_up()
+            plant_objects = PlantManager.get_plants(selected_tile)
+            for selected_plant in plant_objects:
+                selected_plant.age_up()
     
-    def get_plants(self, given_tile: Tile):
+    def get_plants(given_tile: Tile):
         tile_plants = []
         for tile_object in given_tile.objects:
             if isinstance(tile_object, Plant):
