d7a699fcee321042eff9d2387793c6cafec29a10
Nathan Steele <nsteele1031@gmail.com>
2026-06-08 08:45:27 -0400

    Added docstrings to class

--- Plants/plant.py
+++ Plants/plant.py
@@ -1,14 +1,28 @@
 from Map.world_object import WorldObject
 
 class Plant(WorldObject):
+    """
+    This is a WorldObject that is for plants.
+
+    Instance Variables
+    -\n
+    age (int) : The age of the plant.
+    seed (bool) : This is a flag to see if it's a seedling.
+    """
     def __init__(self, name: str, seed: bool):
         super().__init__(name)
         self.age = 0
         self.seed = seed
     
     def age_up(self):
+        """
+        Increases the age of the plant.
+        """
         self.age += 1
     
     def grown(self):
+        """
+        A check to see if the plant is a seedling and it able to grow.
+        """
         if self.seed and self.age == 5:
             self.seed = not self.seed
