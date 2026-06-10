c8ab7b267d3569c2da193ac7f6f2813d4ccfb835
NathanSteele1031 <nsteele1031@gmail.com>
2026-06-10 17:46:42 -0400

    Put in the logic for the show function.

--- Plants/plant_viewer.py
+++ Plants/plant_viewer.py
@@ -7,8 +7,17 @@ class PlantViewer:
     def show(self, given_map: Map):
         if not isinstance(given_map, Map):
             raise TypeError(f"The passed map value is not a Map class, '{type(given_map)}'")
-        print(" ", end="")
         for y in range(given_map.width):
+            if self.selected_tile == 0 and y == 0:
+                print("(", end="")
+            else:
+                print(" ", end="")
             for x in range(given_map.width):
-                print_index = x + (y*self.width)
-                
+                print_index = x + (y*given_map.width)
+                if self.selected_tile == print_index + 1:
+                    print(given_map.tiles[print_index].show(), end="(")
+                elif self.selected_tile == print_index:
+                    print(given_map.tiles[print_index].show(), end=")")
+                else:
+                    print(given_map.tiles[print_index].show(), end=" ")
+            print()
