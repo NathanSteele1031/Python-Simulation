782c9834275323d987641b550fe0ec8d05103f4f
Nathan Steele <126918498+NathanSteele1031@users.noreply.github.com>
2026-07-14 15:22:30 -0400

    Initialize Person class with hunger and health attributes
    Added attributes for hunger, health, and hands to the Person class. Along with blueprint classes.

--- People/person.py
+++ People/person.py
@@ -1,3 +1,28 @@
 class Person:
   def __init__(self):
+    self.hunger = 0
+    self.health = 100
+    self.right_hand = None
+    self.left_hand = None
+
+  def eat(self):
+    # Check if the item in any hands is a food item
+
+    #Then decrease hunger if it is food
+    pass
+
+  def pickup_item(self, item):
+    # Check if either hands are empty and set hand values to item value
+    pass
+
+  def drop(self, hand: str):
+    # Returns the item that is in the selected hand from hand perameter
+    pass
+
+  def move(self, direction: str):
+    # Moves self object in the world map. (needs to figure out how map works)
+    pass
+
+  def AI_actions(self):
+    # Perform actions based on needs. (work on after functional person class and implamented to main game.)
     pass
