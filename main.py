05cc28ee959a7758e9ced2ee877ee4b7fc0cb671
Nathan Steele <nsteele1031@gmail.com>
2026-07-28 17:59:13 -0400

    Added a global map.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from Map.map import Map
+from Plants.plant import Plant
+from Plants.plant_manager import PlantManager
 
 MAP_WIDTH = 5
 
 def main():
     global_map = Map(MAP_WIDTH)
+
+    global_map.show()
 
 if __name__ == "__main__":
     main()
